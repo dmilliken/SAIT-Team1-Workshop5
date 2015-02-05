4770c8c17956a9eb35b6649cdfa41675f07a79a6
BenderTheOffender <m_d132003@hotmail.com>
2015-02-05 10:00:13 -0700

    default errors fixed

--- Team1-WorkshopASP/Default.aspx
+++ Team1-WorkshopASP/Default.aspx
@@ -1,7 +1,7 @@
 ﻿<%@ Page Title="Home Page" Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
 
 <asp:Content runat="server" ID="BodyContent" ContentPlaceHolderID="MainContent">
-    <!--h3>Hello! Please select your surname:
+   <%-- <!--h3>Hello! Please select your surname:
         <!-- Dropdown menu to select the customer ->
         <asp:DropDownList ID="ddlChooseCustomer" runat="server" AutoPostBack="True" DataSourceID="CustomerDropDownData" 
             DataTextField="CustLastName" DataValueField="CustomerID" OnSelectedIndexChanged="ddlChooseCustomer_SelectedIndexChanged">
@@ -18,7 +18,7 @@
     
     <!-- Upon selection, display the greeting panel  -->
 
-    <!-- This is Where thoe Home page link goes to to -->
+    <!-- This is Where thoe Home page link goes to to -->--%>
 
    <h1>Place holder for final Travel Experts Home Page</h1> 
 
--- Team1-WorkshopASP/Default.aspx.cs
+++ Team1-WorkshopASP/Default.aspx.cs
@@ -18,32 +18,32 @@ public partial class _Default : Page
             Session["loggedin"] = false;
         }
     }
-    protected void ddlChooseCustomer_SelectedIndexChanged(object sender, EventArgs e)
-    {
-        // DM: When the drop down list is changed, greet the customer, then make the default page links and customer information visible.
+    //protected void ddlChooseCustomer_SelectedIndexChanged(object sender, EventArgs e)
+    //{
+    //    // DM: When the drop down list is changed, greet the customer, then make the default page links and customer information visible.
 
-        // show the panel
-        pnlGreeting.Visible = true;
+    //    // show the panel
+    //    pnlGreeting.Visible = true;
 
-        Session.Clear(); // clear the session variables
-        Customer customer = new Customer();
-        customer = null;
-        // get the customer ID from the box
+    //    Session.Clear(); // clear the session variables
+    //    Customer customer = new Customer();
+    //    customer = null;
+    //    // get the customer ID from the box
 
-        int custID = Convert.ToInt32(ddlChooseCustomer.SelectedValue);
+    //    //int custID = Convert.ToInt32(ddlChooseCustomer.SelectedValue);
 
-        // store it in a session variable 
-        Session["custID"] = custID;
-        customer = CustomerDB.GetCustomerByID(custID);
+    //    // store it in a session variable 
+    //    Session["custID"] = custID;
+    //    customer = CustomerDB.GetCustomerByID(custID);
 
-        Session["CustFirstName"] = customer.CustFirstName;
-        Session["CustLastName"] = customer.CustLastName;
+    //    Session["CustFirstName"] = customer.CustFirstName;
+    //    Session["CustLastName"] = customer.CustLastName;
 
-        //Response.Redirect(Request.RawUrl);
-        // lblWelcomeName.Text = Convert.ToString(ddlChooseCustomer.SelectedItem);
-        // lblWelcomeName.Text = Convert.ToString(custID);
-        // lblWelcomeName.Text = Convert.ToString(Session["CustFirstName"]) + " " + Convert.ToString(Session["CustLastName"]);
-        lblWelcomeName.Text = customer.CustFirstName.ToString() + " " + customer.CustLastName.ToString();
+    //    //Response.Redirect(Request.RawUrl);
+    //    // lblWelcomeName.Text = Convert.ToString(ddlChooseCustomer.SelectedItem);
+    //    // lblWelcomeName.Text = Convert.ToString(custID);
+    //    // lblWelcomeName.Text = Convert.ToString(Session["CustFirstName"]) + " " + Convert.ToString(Session["CustLastName"]);
+    //    lblWelcomeName.Text = customer.CustFirstName.ToString() + " " + customer.CustLastName.ToString();
          
-    } // end method
+    //} // end method
 }
